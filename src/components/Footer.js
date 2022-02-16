e7a5ebcfbc76241fd9c27262736f15452553cdf2
Rohit Multani <rohitmultani153@gmail.com>
2022-02-16 09:26:52 +0530

    footer added

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import classes from "./Footer.module.css";
+const Footer=()=>{
+    return (
+        <Fragment>
+<div className={classes.footer}>
+    Rohit Multani
+</div>
+        </Fragment>
+    );
+};
+export default Footer;
