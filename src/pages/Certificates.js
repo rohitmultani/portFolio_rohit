ad8e775fae91fa6a4516416c41336aa7afbacba8
Rohit Multani <rohitmultani153@gmail.com>
2022-02-18 09:19:21 +0530

    animations

--- src/pages/Certificates.js
+++ src/pages/Certificates.js
@@ -1,27 +1,32 @@
-import React, { Fragment } from "react";
+import React, { Fragment,useEffect} from "react";
 import classes from "./Certificates.module.css";
-import resume from "../document/Resume.pdf";
+import Aos from "aos";
+import "aos/dist/aos.css"
+
 const Certificates = () => {
+  useEffect(()=>{
+    Aos.init({duration:2000});
+      },[]);
   return (
     <Fragment>
       <div className={classes.block}>
-      <div className={classes.head}>Certificates</div>
+      <div className={classes.head} data-aos="zoom-in-up">Certificates</div>
       <div className={classes.certificates}>
-        <div className={classes.cBlock}>
+        <div className={classes.cBlock} data-aos="zoom-in-down">
           <div className={classes.cName}>Workshop:Machine learning and Artificial intelligence by IBM</div>
           <div className={classes.cCollege}>Cognizance, IIT Roorkee</div>
           <div className={classes.cButton}>
           <a href="https://netcredential.com/verify/nrwr67bdc8"> <button>See Credential</button></a>
           </div>
         </div>
-        <div className={classes.cBlock}>
+        <div className={classes.cBlock} data-aos="zoom-in-up">
           <div className={classes.cName}>Winner_final</div>
           <div className={classes.cCollege}>NSS IIT ROORKEE</div>
           <div className={classes.cButton}>
             <a href="https://netcredential.com/verify/7PPXb06db7">  <button>See Credential</button></a>
           </div>
         </div>
-        <div className={classes.cBlock}>
+        <div className={classes.cBlock} data-aos="zoom-in-down" >
           <div className={classes.cName}>Workshop:Cognizance `21</div>
           <div className={classes.cCollege}>Cognizance, IIT Roorkee</div>
           <div className={classes.cButton}>
