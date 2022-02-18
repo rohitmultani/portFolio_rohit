4e7da02b8fb7558d99c00eacee94d36acf27844a
Rohit Multani <rohitmultani153@gmail.com>
2022-02-18 09:39:37 +0530

    animations

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,17 +1,22 @@
-import React,{Fragment} from "react";
+import React,{Fragment,useEffect} from "react";
 import classes from './Projects.module.css';
 import clock from '../Images/clock.jpeg'
 import calculator from '../Images/calculator.jpeg'
 import userlist from '../Images/userlist.jpeg'
 import github from "../Images/github.png";
 import chrome from "../Images/chrome.png";
+import Aos from "aos";
+import "aos/dist/aos.css"
 const Projects =()=>{
+    useEffect(()=>{
+        Aos.init({duration:1000});
+          },[]);
     return(
     <Fragment>
-              <div className={classes.head}>Projects</div>
+              <div className={classes.head} data-aos="zoom-in-down">Projects</div>
               <div className={classes.Projects}>
-                  <div className={classes.card}>
-                      <img src={clock} alt="clock"></img>
+                  <div className={classes.card} >
+                      <img src={clock} alt="clock" data-aos="fade-up"></img>
                   <div className={classes.hiddenCard}>Alarm Clock(JavaScript)
                   <div>
                       <span><a href="https://github.com/rohitmultani/Alarm-clock" target="_blank"><img src={github} alt="github"/></a></span>
@@ -20,7 +25,7 @@ const Projects =()=>{
                   </div>   
                   </div>
                   <div className={classes.card}>
-                  <img src={calculator} alt="calculator">
+                  <img src={calculator} alt="calculator" data-aos="fade-down">
                   </img>
                   <div className={classes.hiddenCard}>Calculator(JavaScript)
                   <div>
@@ -32,7 +37,7 @@ const Projects =()=>{
                    
                   </div>
                   <div className={classes.card}>
-                  <img src={userlist} alt="use;ist"></img>
+                  <img src={userlist} alt="use;ist" data-aos="fade-up"></img>
                   <div className={classes.hiddenCard}>UserList(React.Js)
                   <div>
                       <span><a href="https://github.com/rohitmultani/user-list"target="_blank"><img src={github} alt="github"/></a></span>
